9ea6e32872d2d194304dd60f3a55de144b065cf6
mzmarlies <marliesclaire@gmail.com>
2022-04-04 16:45:51 -0600

    got the name and photos of dogs to show up on the page using JS

--- scripts/script.js
+++ scripts/script.js
@@ -4,42 +4,13 @@ const dogApp = {};
 // api url (breeds?)
 dogApp.apiUrl = "https://api.thedogapi.com/v1/breeds/";
 
-dogApp.randomPicUrl = "https://dog.ceo/api/breeds/image/random/7";
-
 // api key
 dogApp.apiKey = "3e69176f-30f7-43b0-a73b-9152505ba7d0";
+// storing all the dogs:
+dogApp.dogList = [];
 
 
-
-// define the method to make request to API (appObject.getBreed)
-// dogApp.getImage = () => {
-//   const url = new URL(dogApp.randomPicUrl);
-//   url.search = new URLSearchParams({
-//     "x-api-key": dogApp.apiKey
-//   })
-
-//   // fetch api
-//   fetch(url).then((response) => {
-//     return response.json();
-//   }).then((jsonResponse) => {
-
-//     dogApp.displayPic(jsonResponse);
-//     console.log(jsonResponse);
-//   })
-// };
-
-// dogApp.displayPic = () => {
-//   const imgContainer = document.querySelector('.img-container');
-
-//   dogImages.forEach((dog) => {
-//     const imgElement = document.createElement('img');
-//     // imgElement.src = '';
-
-//   })
-// }
-
-
-
+// pulling the dog breeds and info from API
 dogApp.getBreed = () => {
   const url = new URL(dogApp.apiUrl);
   url.search = new URLSearchParams({
@@ -51,14 +22,16 @@ dogApp.getBreed = () => {
     return response.json();
   }).then((jsonResponse) => {
 
-    console.log(jsonResponse);
-    dogApp.displayInfo(jsonResponse);
+    // console.log(jsonResponse);
+    dogApp.dropdownBreeds(jsonResponse);
+    // dogApp.getUserInput(jsonResponse);
+    dogApp.dogList = jsonResponse;
   })
 };
 
-// when search is succesful, display image to screen
 
-dogApp.displayInfo = (dogObjects) => {
+// populate the dropdown wiht dog breeds
+dogApp.dropdownBreeds = (dogObjects) => {
 
   const dropdown = document.querySelector("select");
 
@@ -67,22 +40,70 @@ dogApp.displayInfo = (dogObjects) => {
 
     optionElement.value = dogObject.name;
     optionElement.innerHTML = dogObject.name;
-
+    
     // optionElement.appendChild();
     dropdown.appendChild(optionElement);
   })
-
-  const userSelection = document.querySelector("option");
-
-  userSelection.addEventListener("click", function(event){
-    event.preventDefault()
-
-    const dogChoice = userSelection.value
-    console.log(dogChoice)
-  })
-
-
 }
+
+// display the user input on change
+dogApp.getUserInput = function() {
+  document.querySelector('#dog-breeds').addEventListener('change', function(){
+    document.querySelector('#intial-results').innerHTML = "";
+    const breed = this.value;
+    dogApp.getBreed(breed);
+    // console.log(breed)
+    
+    const title = document.createElement('h2');
+    // title.innerText = "";
+    title.innerText = breed;
+    // console.log(title)
+
+  
+    // making a div for a title:
+    const titleDiv = document.createElement('div')
+    titleDiv.classList.add('title-div');
+    titleDiv.appendChild(title);
+    document.querySelector('#intial-results').appendChild(titleDiv);
+
+
+    // pulling dog info from array:
+    const selectedDog = dogApp.dogList.filter(function(dog) {
+      return breed === dog.name;
+    })[0]
+    // console.log(selectedDog);
+    
+    // creating an image for the dog:
+    const image = document.createElement('img');
+    image.classList.add('image-result')
+    image.src = selectedDog.image.url;
+
+
+    // creating an image container:
+    // const imgDiv = document.createElement('div')
+    // imgDiv.classList.add('img-div');
+    // imgDiv.appendChild(image);
+    document.querySelector('#img-div').appendChild(image);
+  })
+}
+
+// trying to get info but not working
+// need to decide whether to use css for this instead.
+dogApp.getDogInfo = function() {
+  document.querySelector('#img-div').addEventListener('click', function() {
+    document.querySelector('#intial-results').innerHTML = "";
+    const breed = this.value;
+    // dogApp.getBreed(breed);
+
+    console.log(breed)
+    // const selectedDogInfo = dogApp.dogList.filter(function(dog) {
+    //   return breed === dog.name;
+    // })[0]
+    // console.log(selectedDogInfo);
+  })
+}
+
+
 
 // on click event, show image for specified breed
 
@@ -96,6 +117,8 @@ dogApp.displayInfo = (dogObjects) => {
 
 dogApp.init = () => {
   dogApp.getBreed();
+  dogApp.getUserInput();
+  dogApp.getDogInfo();
 }
 
 dogApp.init();
